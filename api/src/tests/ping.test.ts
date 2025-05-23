827ce6955e1545bf37de27448752d06fb1e57ad1
matteobnvl <matteobonneval19@gmail.com>
2025-05-23 13:58:19 +0200

    feat: add test authenticate

--- api/src/tests/ping.test.ts
+++ api/src/tests/ping.test.ts
@@ -1,9 +1,29 @@
 import request from "supertest";
-const baseUrl = "https://owl-writey.hemit.fr/api";
+const baseUrl = "https://owl-writey.hemit.fr/api"
+const email = 'pseudo@gmail.com'
+const password = 'Test1234@'
+
+async function authenticate(login: string, pwd: string): Promise<string> {
+    const url = 'https://www.googleapis.com/';
+    const response = await request(url).post("/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDpdYdgvEwYIKGr_rmh37DipL3djZ-KF3k")
+    .send({
+        "email": login,
+        "password": pwd,
+        "returnSecureToken": true
+    });
+    expect(response.status).toBe(200);
+    return response.body.idToken;
+}
 
 describe("External API Tests", () => {
     it("should return 200 OK for the root endpoint", async () => {
-        const response = await request(baseUrl).get("/ping");
-        expect(response.status).toBe(200);
-    });
-});
+        const response = await request(baseUrl).get("/ping")
+        expect(response.status).toBe(200)
+    })
+
+    it("should return 200 OK for the root endpoint", async () => {
+        const token = await authenticate(email, password)
+        const response = await request(baseUrl).get("/exercises").set("Authorization", `Bearer ${token}`)
+        expect(response.status).toBe(200)
+    })
+})
