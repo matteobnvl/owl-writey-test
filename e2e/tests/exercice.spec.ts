efe5bde90d544d299b5860e193856a327bf459d4
nico servary <nicolas.servary123@gmail.com>
2025-06-02 14:27:11 +0200

    [Feat] connexion, create and delete exercice

--- e2e/tests/exercice.spec.ts
+++ e2e/tests/exercice.spec.ts
@@ -19,6 +19,26 @@ test('create exercice', async ({ page }) => {
     await expect(page.getByRole('heading', { level: 1, name: 'Test e2e' })).toBeVisible();
 });
 
+test('play exercice', async ({ page }) => {
+    await login(page);
+    await expect(page.getByRole('heading', { level: 1, name: 'Mes Romans' })).toBeVisible();
+    const element = page.locator('owl-dashboard-exercise-card').filter({ hasText: 'Test e2e' });
+    await element.getByRole('link', { name: 'Jouer' }).click({ trial: false });
+
+    await page.getByRole('button', { name: ' À mon tour !' }).click({ trial: false });
+    await page.locator('.NgxEditor__Content').fill('il a fini par mourir dans un grand éclat de rire');
+    await page.getByRole('button', { name: 'Soumettre' }).click({ trial: false });
+
+    await expect(page.locator('owl-exquisite-corpse-details').filter({ hasText: 'il a fini par mourir dans un grand éclat de rire' })).toBeVisible();
+
+    await page.getByTitle('Partager').click({ trial: false });
+    const shareLink = await page.getByRole('button', { name: 'Copier' }).getAttribute('href');
+    console.log('Share Link:', shareLink);
+
+
+
+});
+
 test('delete exercice', async ({ page }) => {
     await login(page);
     await expect(page.getByRole('heading', { level: 1, name: 'Mes Romans' })).toBeVisible();
--- e2e/tests/helpers/login.ts
+++ e2e/tests/helpers/login.ts
@@ -2,9 +2,9 @@
 import { Page } from '@playwright/test';
 
 
-export async function login(page: Page) {
+export async function login(page: Page, email: string = 'pseudo@gmail.com', password: string = 'Test1234@'): Promise<void> {
     await page.goto('/login');
-    await page.getByRole('textbox', { name: 'Email' }).fill('pseudo@gmail.com');
-    await page.getByRole('textbox', { name: 'Mot de passe' }).fill('Test1234@');
+    await page.getByRole('textbox', { name: 'Email' }).fill(email);
+    await page.getByRole('textbox', { name: 'Mot de passe' }).fill(password);
     await page.getByRole('button', { name: 'Connexion' }).nth(1).click({ trial: false });
 }
